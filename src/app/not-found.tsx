b37cbe32c4c7fcd6b876392adb23b0b4360e4f4f
diana <GDI.1999@yandex.ru>
2025-06-12 15:00:53 +0300

    feat: 404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,8 @@
-import Link from "next/link";
 import { BackgroundImage } from "@/components/BackgroundImage";
 import back3 from "@/shared/images/back480.jpg";
 import back1 from "@/shared/images/back1920.jpg";
 import back2 from "@/shared/images/back1024.jpg";
+import { Grid, Typography } from "@mui/material";
 
 export default function NotFound() {
   const sources = [
@@ -16,11 +16,10 @@ export default function NotFound() {
     }
   ];
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <Grid container direction={"column"} sx={{ alignItems: "center", justifyContent: "center" }}>
+      <Typography variant={"h1"} className={"header-404"}>404</Typography>
+      <Typography variant={"p_24/13"}>Sorry, there was a problem, please come back later</Typography>
       <BackgroundImage mainImageSrc={back3.src} alt={"background image"} sources={sources} />
-    </div>
+    </Grid>
   );
 }
